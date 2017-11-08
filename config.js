1957eb0247a343d4939a53f344c8c8ce0afd9f7f
giamtrot <giamtrot@hotmail.com>
2017-11-08 14:24:47 +0100

    Prima Versione Push

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAco6Sp4Y:APA91bFQREAG6sina6_53muVq8-fnaJclangTnPPD-z9bheqWO0C6h3GOQ0hZBLbJTTeollzZkDI0s1fJ069PONg3cttkmzB2H-IM0Vpse55yff5m5UuCQjs-4-DuSQHYgptfYmPa15F'
+};
